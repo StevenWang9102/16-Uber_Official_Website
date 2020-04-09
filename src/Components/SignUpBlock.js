9d6e8793618013a003c3a589058012673b12e3f7
Sibo Wang <375973811@qq.com>
2020-04-10 10:25:22 +1200

    sign arrow lose and display

--- src/Components/SignUpBlock.js
+++ src/Components/SignUpBlock.js
@@ -6,8 +6,12 @@ export const SignUpBlock = (props) => {
     <section id='sign-up-block' className='col-sm-9 col-md-6 col-lg-6'>
       <h1>
         {props.title}
-        <img src={arrow} alt=''/>
       </h1>
+      <div className="container">
+        <div className="block_box1"></div>
+        <div className="block_box2"></div>
+        <img src={arrow} alt=''/>
+      </div>
     </section>
   );
 };
